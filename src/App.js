9386630a6f35d590dafa8c5e9b86ecd1b2675793
ExcuseMeImJack <ExcuseMeImJack@gmail.com>
2025-02-17 13:42:01 -0800

    Added initial layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Your Game!</h1>
+      {/* Your game components go here */}
+    </div>
+  );
+}
+
+export default App;
