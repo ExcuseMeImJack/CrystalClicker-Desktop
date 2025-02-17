487964e7b0f7ecda651ac22655c821d5b8b6f263
ExcuseMeImJack <ExcuseMeImJack@gmail.com>
2025-02-17 13:34:53 -0800

    Added Initial

---
+++ public/electron.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // If using Electron 12+ this might need to be true.
+    },
+  });
+
+  win.loadURL('http://localhost:3000'); // Change this if building for production
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
